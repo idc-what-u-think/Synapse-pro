95f29121f5e9b9d3bd5a98decd0596860f78e2be
firekid-prime <ahmedayomide1005@gmail.com>
2025-08-31 03:04:53 +0100

    Create gameEnums.js

---
+++ src/types/gameEnums.js
@@ -0,0 +1,34 @@
+const Game = {
+  FF: "Free Fire",
+  CODM: "Call of Duty Mobile"
+};
+
+const PlayStyle = {
+  AGGRESSIVE: "Aggressive",
+  BALANCED: "Balanced",
+  PASSIVE: "Passive"
+};
+
+const ExperienceLevel = {
+  BEGINNER: "Beginner",
+  INTERMEDIATE: "Intermediate",
+  ADVANCED: "Advanced",
+  PRO: "Professional"
+};
+
+const FingerCount = {
+  TWO: "2 Fingers",
+  THREE: "3 Fingers",
+  FOUR: "4 Fingers",
+  FIVE: "5 Fingers",
+  SIX: "6 Fingers",
+  SEVEN: "7 Fingers",
+  EIGHT: "8 Fingers"
+};
+
+module.exports = {
+  Game,
+  PlayStyle,
+  ExperienceLevel,
+  FingerCount
+};
