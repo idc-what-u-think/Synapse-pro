d56387583a9e1999e6d7504497c07deff7dbddd7
idc-what-u-think <ahmedayomide1000@gmail.com>
2025-10-25 20:46:31 +0100

    Create interactionCreate.js

---
+++ src/events/interactionCreate.js
@@ -0,0 +1,26 @@
+const { handleSetupButtons } = require('./setupButtonHandler');
+
+module.exports = {
+    name: 'interactionCreate',
+    async execute(interaction) {
+        if (interaction.isChatInputCommand()) {
+            const command = interaction.client.commands.get(interaction.commandName);
+
+            if (!command) {
+                console.error(`No command matching ${interaction.commandName} was found.`);
+                return;
+            }
+
+            try {
+                await command.execute(interaction);
+            } catch (error) {
+                console.error(`Error executing ${interaction.commandName}`);
+                console.error(error);
+            }
+        }
+
+        if (interaction.isButton()) {
+            await handleSetupButtons(interaction);
+        }
+    },
+};
